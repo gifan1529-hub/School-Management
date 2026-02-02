f533ad2c91e489e0b1da27a08fd98a74bf75200c
gifan1529-hub <gifan1529@gmail.com>
2026-02-02 14:34:13 +0700

    feat : screen jadwal mengjar (masih dummy)

---
+++ composeApp/src/commonMain/kotlin/com/example/schoolmanagement/UI/Screen/Teacher/JadwalMengajarScreen.kt
@@ -0,0 +1,157 @@
+package com.example.schoolmanagement.UI.Screen.Teacher
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.schoolmanagement.UI.Component.ScheduleItem
+import com.example.schoolmanagement.ViewModel.ScheduleViewModel
+import com.example.schoolmanagement.getTodaDayName
+import com.example.schoolmanagement.getTodayDate
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+fun JadwalMengajarScreen (
+    navController: NavController,
+    viewModel: ScheduleViewModel = koinViewModel()
+) {
+
+    val userClass by viewModel.userClass.collectAsState()
+    val schedules by viewModel.schedules.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+
+    val primaryBlue = Color(0xFF0066FF)
+
+    val todayName = getTodaDayName()
+    val todaySchedules = schedules.filter { it.day == todayName }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFF5F7FA))
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = primaryBlue,
+                        shape = RoundedCornerShape(bottomStart = 32.dp, bottomEnd = 32.dp)
+                    )
+                    .padding(horizontal = 24.dp, vertical = 40.dp)
+            ) {
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    IconButton(
+                        onClick = { navController.popBackStack() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
+                    }
+
+                    Text(
+                        text = "Jadwal",
+                        color = Color.White,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+            Card(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .offset(y = (-30).dp)
+                    .fillMaxSize(),
+                shape = RoundedCornerShape(16.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(
+                        text = "$todayName, ${getTodayDate()}",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        color = primaryBlue
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        text = userClass,
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 13.sp,
+                        color = Color.Black
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    if (isLoading) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            CircularProgressIndicator(
+                                color = primaryBlue
+                            )
+                        }
+                    } else if (todaySchedules.isEmpty()) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("Tidak ada jadwal untuk hari $todayName", color = Color.Gray)
+                        }
+                    } else {
+                        LazyColumn(
+                            verticalArrangement = Arrangement.spacedBy(12.dp)
+                        ) {
+                            items(todaySchedules) { item ->
+                                ScheduleItem(
+                                    time = "${item.time_in.take(5)} - ${item.time_out.take(5)}",
+                                    subject = item.subject,
+                                    teacher = item.teacher?.name ?: "gada"
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
